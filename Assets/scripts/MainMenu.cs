d43c9f08db06a19462974f8efd57c11b6dea2814
UsernameisKoo <syonkoo@naver.com>
2026-04-02 11:00:37 +0900

    Opening Scene

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -1,19 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MainMenu: MonoBehaviour {
 
-    public void PlayGame () 
+public class MainMenu : MonoBehaviour
+{
+    void Update()
     {
-        Debug.Log("xd");
-        SceneManager.LoadScene (1);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartGame();
+        }
     }
 
-    public void QuitGame ()
+    void StartGame()
     {
-        Debug.Log ("QUIT!");
-        Application.Quit(); 
+        Debug.Log("Start!");
+        SceneManager.LoadScene(7);
     }
-
 }
